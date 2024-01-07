49632ece06d046306fbeea20882a839455be6aee
juno.park <pjguam0@naver.com>
2024-01-07 18:03:07 +0900

    First commit

--- find_file.py
+++ find_file.py
@@ -1,47 +1,18 @@
 #!/usr/bin/python3
 #encoding=utf-8
 
+#첫째 줄의 내용은 프로그램 실행 시 해당 경로의 프로그램을 활용하라는 참고이다.
+#두 번째 줄의 내용은 인코딩 오류를 막기 위한 참고이다.
 
-
-# author : Juno Park
-
-
-# def add():
-#     a: int = int(input())
-#     b: int = int(input())
-#
-# print(a)
-
-
-# r: read
-# w: write
-# w: append
-
-# f=open('test.txt', 'w')
-# f.write('asdasdasdsd\n')
-# f.write('dsaasdasdsd\n')
-# f.write('sasdasdasdasdsd\n')
-# f.close()
-
-# f = open('test.txt', 'r')
-# res = f.readlines()
-# f.close()
-#
-# for i in res:
-#     print(i.strip('\n'))
-
-# with open('test.txt', 'w') as f:
-#     f.write('tikrmedkd\n')
-# print()
-
-# 경로를 입력하면 해당 파일 리스트를 보여주는 스크립트
-# 1. 스탠다드 모듈
-# 2. 써드파티 모듈
-# 3. 본인이 작성한 모듈
+# Find 'ok' in files and write on 'result.txt'
 
 import sys
 import pathlib
+import re
+import time
+import os
 
+keyword = input("검색어를 입력하세요 : ")
 
 def get_path():
     home = pathlib.Path.home()
@@ -56,19 +27,20 @@ def get_path():
                 # fp.readlines() --> list
                 try:
                     for rl in fp.readlines():
-                        res = re.search(r'\s+(ok)\s+', rl)
+                        res = re.search(keyword, rl) # 'ok'자리에 다른 문자열 입력 가능
                         # ok 단어를 못 찾았다면,
                         if res is None:
                             continue
                         with open(home_path, 'a') as fp2:
-                            fp2.write(rl)
+                            fp2.write('\n파일명 : {0} \n경로 : {1} \n내용 : {2}\n'.format(i.name, i.as_posix(), rl))
                 except UnicodeDecodeError as err:
                     pass
+        with open(home_path, 'a') as today:
+            today.write('찾은 시간 : {0}\n\n'.format(time.strftime('%c', time.localtime(time.time()))))
+        with open(home_path, 'a') as multiline:
+            multiline.write('='*50)
     except IndexError as err:
         sys.stderr.write('index가 잘못됨')
 
+
 get_path()
-
-
-
-
--- find_ok.py
+++ find_ok.py
@@ -5,7 +5,7 @@
 #두 번째 줄의 내용은 인코딩 오류를 막기 위한 참고이다.
 
 # Find 'ok' in files and write on 'result.txt'
-
+'''
 import sys
 import pathlib
 
@@ -19,5 +19,37 @@ def get_path():
             if 'ok' in map(lambda x: x.strip('\n'), fp.readlines()):
                 with open("/home/rapa/result.txt", 'w') as result:
                     result.write(fp_lst)
+
+get_path()
+'''
+
+
+import sys
+import pathlib
+import re
+
+def get_path():
+    home = pathlib.Path.home()
+    home_path = '{0}/{1}'.format(home.as_posix(), 'result.txt')
+    count = 0
+    try:
+        s_path = pathlib.Path(sys.argv[1])
+        for i in s_path.glob('**/*.py'):
+            if count > 5:
+                break
+            with open(i.as_posix(), 'r') as fp:
+                # fp.readlines() --> list
+                try:
+                    for rl in fp.readlines():
+                        res = re.search(r'\s+(ok)\s+', rl) # 'ok'자리에 다른 문자열 입력 가능
+                        # ok 단어를 못 찾았다면,
+                        if res is None:
+                            continue
+                        with open(home_path, 'a') as fp2:
+                            fp2.write('파일명 : {0} \n경로 : {1} \n내용 : {2} \n'.format(i.name, i.as_posix(), rl))
+                except UnicodeDecodeError as err:
+                    pass
+    except IndexError as err:
+        sys.stderr.write('index가 잘못됨')
 
 get_path()
